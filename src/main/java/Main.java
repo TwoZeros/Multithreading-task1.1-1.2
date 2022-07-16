2524c66cd2ba97c19f5f740eb7628c600af11c5e
Kirill <pak.kiruha@yandex.ru>
2022-07-17 02:25:07 +0300

    task 1.2 comlite

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,57 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+
 public class Main {
 
     private static final int DURATION_SLEEP = 15000;
+    private static final int THREAD_SLEEP = 2000;
+    private static final int COUNT_THREAD = 4;
 
-    public static void main(String[] args) {
-        ThreadGroup group = new ThreadGroup("thread");
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
 
-        new MyThread(group).start();
-        new MyThread(group).start();
-        new MyThread(group).start();
-        new MyThread(group).start();
+        Callable<Integer> helloCallable = () -> {
+            final int maxIteration = 5;
+            int counter = 0;
+            try {
+                while (!Thread.currentThread().isInterrupted() && counter < maxIteration) {
+                    Thread.sleep(THREAD_SLEEP);
+                    counter++;
+                    System.out.printf("Я %s. Всем привет!\n", Thread.currentThread().getName());
+                }
+            } catch (InterruptedException ignored) {
+            } finally {
+                System.out.printf("%s завершен\n", Thread.currentThread().getName());
+            }
+            return counter;
+        };
 
-        try {
-            Thread.sleep(DURATION_SLEEP);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println("Завершаю все потоки");
-            group.interrupt();
+        List<Callable<Integer>> callableList = new ArrayList<>();
+
+        // Создаем пул задач в цикле
+        for (int i = 0; i < COUNT_THREAD; i++) {
+            callableList.add(helloCallable);
         }
+        // Запускаем задания
+        //Получаем один результат
+//      ExecutorService executorService1 = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+//      Integer count = executorService1.invokeAny(callableList);
+//      System.out.printf("Самый быстрый поток выполнился %d раз \n", count);
+//      executorService1.shutdown();
 
+        //Запускаем задания
+        //Получаем все значения и выводим их
+        ExecutorService executorService2 = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<Future<Integer>> result = executorService2.invokeAll(callableList);
+        System.out.println("Результаты:");
+        result.forEach((i) -> {
+            try {
+                System.out.println(i.get());
+            } catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+        });
+        executorService2.shutdown();
     }
 
 }
